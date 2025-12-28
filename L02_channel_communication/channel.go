85d454ed24455c1681881c3eac161ec5b6fe4ce8
T <test@test.test>
2025-12-28 14:22:45 +0100

    Cleanup

--- L02_channel_communication/channel.go
+++ L02_channel_communication/channel.go
@@ -8,26 +8,22 @@ import (
 
 func main() {
 	messages := make(chan string)
-	var wg sync.WaitGroup // Hilft uns zu warten, bis alle Worker fertig sind
-	count := 3            // Wir testen es mal mit 3 Workern
-
-	fmt.Println("Start")
+	var wg sync.WaitGroup
+	count := 3
 
 	for i := 1; i <= count; i++ {
-		wg.Add(1)         // Ein Worker kommt hinzu
-		go func(id int) { // id als Parameter übergeben!
-			defer wg.Done() // Signalisiert am Ende: "Ich bin fertig"
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
 
 			fmt.Printf("Worker %d: Sleeping...\n", id)
-			time.Sleep(2 * time.Second)
+			time.Sleep(1 * time.Second)
 
 			messages <- fmt.Sprintf("Message from Go routine %d!", id)
 			fmt.Printf("Worker %d: Message sent\n", id)
 		}(i)
 	}
 
-	// WICHTIG: In einer extra Go-Routine auf die Worker warten
-	// und DANN den Channel schließen.
 	go func() {
 		wg.Wait()
 		close(messages)
@@ -37,6 +33,4 @@ func main() {
 	for msg := range messages {
 		fmt.Printf("Main: Received %s\n", msg)
 	}
-
-	fmt.Println("Main: Alle Nachrichten empfangen, Ende.")
 }
